86eb8e44c49b66451c645d92a7da59a1585452cc
Sunkiti <elena.bulavkina@gmail.com>
2023-08-01 22:25:57 +0300

    corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './style/App.scss';
 import Prices from "./components/Prices";
-import prices from '../src/data/prices.json';
+
 
 function App() {
   return (
     <>
-      <Prices></Prices>
+      <Prices />
     </>
   );
 }
--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -1,22 +1,22 @@
 import prices from '../data/prices.json';
-import "./style.module.scss";
+import styles from './style.module.scss';
 
 export default function Price() {
     return (
-        <div className="price_wrapper">
+        <div className={styles.price_wrapper}>
             {prices.map((item, index) => (
-                <div className={"price" + (item.isSelected ? " selected" : "")} key={index}>
-                    <div className={"price_title" + (item.bgcolor ? ` ${item.bgcolor}` : "")}>{item.title}</div>
-                    <div className={"price_price" + (item.bgcolor ? ` light_${item.bgcolor}` : "")}>
-                        <p className='price_currency'>руб.</p>
-                        <p className='price_number'>{item.price}</p>
-                        <p className='price_month'>/мес.</p>
+                <div className={styles.price + (item.isSelected ? `${styles.selected}` : "")} key={index}>
+                    <div className={styles.price_title + (item.bgcolor ? ` ${styles[item.bgcolor]}` : "")}>{item.title}</div>
+                    <div className={styles.price_price + (item.bgcolor ? ` ${styles['light_' + item.bgcolor]}` : "")}>
+                        <p className={styles.price_currency}>руб.</p>
+                        <p className={styles.price_number}>{item.price}</p>
+                        <p className={styles.price_mont}>/мес.</p>
                     </div>
-                    <div className="price_speed">до {item.speed} Мбит/сек.</div>
-                    <div className="price_traffic">Обьем включенного траффика не ограничен</div>
+                    <div className={styles.price_speed}>до {item.speed} Мбит/сек.</div>
+                    <div className={styles.price_traffic}>Обьем включенного траффика не ограничен</div>
                 </div>
             ))
             }
-        </div>
+        </div >
     );
 }
